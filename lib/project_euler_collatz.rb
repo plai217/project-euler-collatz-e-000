ba71e9577be7c090902ca0549fd3f0ce6925cea4
plai217 <philiplai217@gmail.com>
2015-10-27 13:47:02 -0400

    Done.

--- lib/project_euler_collatz.rb
+++ lib/project_euler_collatz.rb
@@ -1,14 +1,38 @@
 def even_next(n)
+  n = n/2
 end
 
 def odd_next(n)
+  n = n*3 + 1
 end
 
 def next_value(n)
+  if n.even?
+    even_next(n)
+  else
+    odd_next(n)
+  end
 end
 
 def collatz(n)
+  
+  array = [n] 
+  until n == 1
+    array << next_value(n)
+    n = next_value(n)
+  end
+  array 
+
 end
 
 def longest_collatz
+  longest = 0
+  startnumber = 1
+  1.upto(1000000).each do |x|
+    if collatz(x).length > longest
+      longest = collatz(x).length
+      startnumber = x
+    end
+  end
+  startnumber
 end
